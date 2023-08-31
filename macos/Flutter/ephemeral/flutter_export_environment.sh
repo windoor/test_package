4343d09f6afe711d2998d327b0ce6dc0d91fca21
1ucky <huangjimin@zuzuche.com>
2023-08-31 09:56:38 +0800

    test

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/x1aoyE/Library/Application Support/fvm/versions/3.7.1"
+export "FLUTTER_APPLICATION_PATH=/Users/x1aoyE/Desktop/111232/test_package"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
